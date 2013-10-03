366c7a03d9bca6b1e2bc4265545da8486dbb8605
cspears2002 <cspears2002@yahoo.com>
2013-10-03 16:32:19 -0700

    Trying to implement identifyWin function.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,7 +17,60 @@ angular.module('newTicApp')
 
 		if ($scope.radio == 2 && this.box != "X")
 			this.box = "O";
+
+		$scope.identifyWin($scope.ticTacToe)
   	};
+
+	$scope.identifyWin = function(cellArray) {
+		console.log(cellArray)
+	
+		// Test diagonals
+		if (cellArray[1][1] != "") {
+			if ((cellArray[0][0] == cellArray[1][1] &&
+				 cellArray[1][1] == cellArray[2][2]) ||
+				(cellArray[0][2] == cellArray[1][1] &&
+			 	 cellArray[1][1] == cellArray[2][0])) 
+			{
+				$scope.changeBgClr;		
+			}
+		}
+
+		// Test columns.
+		for(var c = 1; c <= 3; ++c)
+		{
+			if(cellArray[0][c-1] == cellArray[1][c-1]  && 
+			   cellArray[1][c-1] == cellArray[2][c-1]  && 
+		   	   cellArray[0][c-1] != "")
+			{
+				$scope.changeBgClr;
+			}
+		}
+
+		// Test rows.
+		for(var r = 1; r <= 3; ++r)
+		{
+			if(cellArray[r-1][0] == cellArray[r-1][1]  && 
+		   	   cellArray[r-1][1] == cellArray[r-1][2]  && 
+		   	   cellArray[r-1][0] != "")
+			{
+				$scope.changeBgClr;
+			}
+		}
+	};
+
+	$scope.changeBgClr = function() {
+
+		radio_container_1 = document.getElementsByClassName("player_1_radio")[0];
+		radio_container_2 = document.getElementsByClassName("player_2_radio")[0];
+
+		if ($scope.radio == 1) {
+			radio_container_1.style.backgroundColor = '#ffff11';
+		}
+
+		if ($scope.radio == 2) {
+			radio_container_2.style.backgroundColor = '#ffff11';
+		}
+	};
 
   });
 
@@ -28,64 +81,4 @@ function resetGame(element){
 	document.getElementById("reset_popup").style.display = "block";
 }
 
-function changeBgClr() {
 
-	var rd_plyer_1 = document.getElementById('player_1');
-	var rd_plyer_2 = document.getElementById('player_2');
-
-	radio_container_1 = document.getElementsByClassName("player_1_radio")[0];
-	radio_container_2 = document.getElementsByClassName("player_2_radio")[0];
-
-	if (rd_plyer_1.checked) {
-		radio_container_1.style.backgroundColor = '#ffff11';
-	}
-
-	if (rd_plyer_2.checked) {
-		radio_container_2.style.backgroundColor = '#ffff11';
-	}
-}
-
-function identifyWin(cellArray) {
-	
-	for (var r = 1; r <= 3 ; ++r)
-	{
-		for(var c = 1; c <= 3; ++c) 
-		{
-			var div_id = "box" + r + "_" + c;
-			cellArray[r-1][c-1] = document.getElementById(div_id).innerHTML;
-		}
-	}
-	
-	// Test diagonals
-	if (cellArray[1][1] != "") {
-		if ((cellArray[0][0] == cellArray[1][1] &&
-			 cellArray[1][1] == cellArray[2][2]) ||
-			(cellArray[0][2] == cellArray[1][1] &&
-			 cellArray[1][1] == cellArray[2][0])) 
-		{
-			changeBgClr();		
-		}
-	}
-
-	// Test columns.
-	for(var c = 1; c <= 3; ++c)
-	{
-		if(cellArray[0][c-1] == cellArray[1][c-1]  && 
-		   cellArray[1][c-1] == cellArray[2][c-1]  && 
-		   cellArray[0][c-1] != "")
-		{
-			changeBgClr();
-		}
-	}
-
-	// Test rows.
-	for(var r = 1; r <= 3; ++r)
-	{
-		if(cellArray[r-1][0] == cellArray[r-1][1]  && 
-		   cellArray[r-1][1] == cellArray[r-1][2]  && 
-		   cellArray[r-1][0] != "")
-		{
-			changeBgClr();
-		}
-	}
-}
