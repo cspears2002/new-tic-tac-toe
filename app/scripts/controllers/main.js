b2ba0cab9ed769a02cd60339ae47496e44f18e25
cspears2002 <cspears2002@yahoo.com>
2013-10-04 11:16:17 -0700

    Updated identifyWin to deal with references.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -27,11 +27,11 @@ angular.module('newTicApp')
 
 	$scope.identifyWin = function(cellArray) {
 		// Test diagonals
-		if (cellArray[1][1] != "") {
-			if ((cellArray[0][0] == cellArray[1][1] &&
-				 cellArray[1][1] == cellArray[2][2]) ||
-				(cellArray[0][2] == cellArray[1][1] &&
-			 	 cellArray[1][1] == cellArray[2][0])) 
+		if (cellArray[1][1].val != "") {
+			if ((cellArray[0][0].val == cellArray[1][1].val &&
+				 cellArray[1][1].val == cellArray[2][2].val) ||
+				(cellArray[0][2].val == cellArray[1][1].val &&
+			 	 cellArray[1][1].val == cellArray[2][0].val)) 
 			{
 				$scope.changeBgClr();		
 			}
@@ -40,9 +40,9 @@ angular.module('newTicApp')
 		// Test columns.
 		for(var c = 1; c <= 3; ++c)
 		{
-			if(cellArray[0][c-1] == cellArray[1][c-1]  && 
-			   cellArray[1][c-1] == cellArray[2][c-1]  && 
-		   	   cellArray[0][c-1] != "")
+			if(cellArray[0][c-1].val == cellArray[1][c-1].val  && 
+			   cellArray[1][c-1].val == cellArray[2][c-1].val  && 
+		   	   cellArray[0][c-1].val != "")
 			{
 				$scope.changeBgClr();
 			}
@@ -51,9 +51,9 @@ angular.module('newTicApp')
 		// Test rows.
 		for(var r = 1; r <= 3; ++r)
 		{
-			if(cellArray[r-1][0] == cellArray[r-1][1]  && 
-		   	   cellArray[r-1][1] == cellArray[r-1][2]  && 
-		   	   cellArray[r-1][0] != "")
+			if(cellArray[r-1][0].val == cellArray[r-1][1].val && 
+		   	   cellArray[r-1][1].val == cellArray[r-1][2].val && 
+		   	   cellArray[r-1][0].val != "")
 			{
 				$scope.changeBgClr();
 			}
