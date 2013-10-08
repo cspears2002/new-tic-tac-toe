b6060c139f711f000482443b256b0bcb1fb67fa9
cspears2002 <cspears2002@yahoo.com>
2013-10-08 15:10:56 -0700

    Moved AboutCtrl to a separate file (app/scripts/controllers/about.js).

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('newTicApp')
+  .controller('AboutCtrl', function ($scope, $location) {
+  	$scope.blog = 'http://chrisspearswebdev.blogspot.com/';
+	$scope.twitterHandle = '@cspears2002';
+	$scope.linkedIn = 'http://www.linkedin.com/pub/christopher-spears/6/580/945'
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,5 @@
 'use strict';
 
-function AboutCtrl($scope, $location) {
-	$scope.blog = 'http://chrisspearswebdev.blogspot.com/';
-	$scope.twitterHandle = '@cspears2002';
-	$scope.linkedIn = 'http://www.linkedin.com/pub/christopher-spears/6/580/945'
-}
-
 angular.module('newTicApp')
   .controller('MainCtrl', function ($scope, $location) {
 
