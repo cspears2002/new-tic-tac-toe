440e957c384e40d144ca4c804bda59473c5ae386
cspears2002 <cspears2002@yahoo.com>
2013-10-10 11:46:25 -0700

    Added promises.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,166 +6,172 @@ angular.module('newTicApp')
     // Create a queue and hook to firebase.
     $scope.queue = {};
     var dbQueue = new Firebase("https://fire-cspears2002-newtic.firebaseio.com/queue/");
-  	angularFire(dbQueue, $scope, "queue", {});
+  	var queuePromise = angularFire(dbQueue, $scope, "queue", {});
 
+  	queuePromise.then (function () {
 
-    // Hook up Firebase and build a room.
-    $scope.ticTacToe = [
-    	[{val:''},{val:''},{val:''}],
-    	[{val:''},{val:''},{val:''}],
-    	[{val:''},{val:''},{val:''}]
-    ];
+    	// Hook up Firebase and build a room.
+    	$scope.ticTacToe = [
+    		[{val:''},{val:''},{val:''}],
+    		[{val:''},{val:''},{val:''}],
+    		[{val:''},{val:''},{val:''}]
+    	];
 
-    $scope.turn = 1;
-    $scope.numPlayers = 0;
-  	$scope.room = {
-  		board: $scope.ticTacToe,
-  		turn: $scope.turn,
-  		numPlayers: $scope.numPlayers
-  	}
+    	$scope.turn = 1;
+    	$scope.numPlayers = 0;
+  		$scope.room = {
+  			board: $scope.ticTacToe,
+  			turn: $scope.turn,
+  			numPlayers: $scope.numPlayers
+  		}
 
-  	// Push rooms on to firebase.
-  	var dbRooms = new Firebase("https://fire-cspears2002-newtic.firebaseio.com/rooms/");
-  	angularFire(dbRooms, $scope, "rooms");
-  	var fbRef = dbRooms.push($scope.room);
-  	var roomId = fbRef.name();
-  	// Push on to firebase
-  	dbQueue.push({id: roomId});
+  		// Push rooms on to firebase.
+  		var dbRooms = new Firebase("https://fire-cspears2002-newtic.firebaseio.com/rooms/");
+  		var roomPromise = angularFire(dbRooms, $scope, "rooms");
 
-    // Styles.
-    $scope.winStyle = {};
-    $scope.winStyle2 = {};
-    $scope.resetStyle = {background: 'green'};
+  		roomPromise.then (function(){
 
-    // Sets visibility for windows.
-    $scope.resetVisible = {view: true};
-    $scope.gameOverVisible = {view: false};
-    $scope.startGame = {view: false};
+  			var fbRef = dbRooms.push($scope.room);
+  			var roomId = fbRef.name();
+  			// Push on to firebase
+  			dbQueue.push({id: roomId});
 
-    $scope.addXO = function(cell, turnObj) {
+    		// Styles.
+    		$scope.winStyle = {};
+    		$scope.winStyle2 = {};
+    		$scope.resetStyle = {background: 'green'};
 
-    	if (turnObj.number % 2 == 0)
-    	{
-    		$scope.radio = 2;
-    	}
-    	else
-    	{
-    		$scope.radio = 1;
-    	}
+    		// Sets visibility for windows.
+    		$scope.resetVisible = {view: true};
+    		$scope.gameOverVisible = {view: false};
+    		$scope.startGame = {view: false};
 
-		if ($scope.radio == 1 && cell.val != "O")
-			cell.val = "X";
+    		$scope.addXO = function(cell, turnObj) {
+
+    			if (turnObj.number % 2 == 0)
+    			{
+    				$scope.radio = 2;
+    			}
+    			else
+    			{
+    				$scope.radio = 1;
+    			}
+
+				if ($scope.radio == 1 && cell.val != "O")
+					cell.val = "X";
 	
-		if ($scope.radio == 2 && cell.val != "X")
-			cell.val = "O";
+				if ($scope.radio == 2 && cell.val != "X")
+					cell.val = "O";
 			
-		$scope.identifyWin($scope.ticTacToe, turnObj);
-  	};
+				$scope.identifyWin($scope.ticTacToe, turnObj);
+  			};
 
-	$scope.identifyWin = function(cellArray, turnObj) {
-		if (turnObj.number == 9)
-		{
-			$scope.winStyle = $scope.winStyle2 = {background:'#ffff11'};
-			$scope.resetVisible.view = true;
-		}
-		else
-		{
-			// Test diagonals
-			if (cellArray[1][1].val != "") 
-			{
-				if ((cellArray[0][0].val == cellArray[1][1].val &&
-					 cellArray[1][1].val == cellArray[2][2].val) ||
-					(cellArray[0][2].val == cellArray[1][1].val &&
-			 	 	 cellArray[1][1].val == cellArray[2][0].val)) 
+			$scope.identifyWin = function(cellArray, turnObj) {
+				if (turnObj.number == 9)
 				{
-					$scope.changeBgClr();		
+					$scope.winStyle = $scope.winStyle2 = {background:'#ffff11'};
+					$scope.resetVisible.view = true;
 				}
-			}
-
-			// Test columns.
-			for(var c = 1; c <= 3; ++c)
-			{
-				if(cellArray[0][c-1].val == cellArray[1][c-1].val  && 
-			   	   cellArray[1][c-1].val == cellArray[2][c-1].val  && 
-		   	   	   cellArray[0][c-1].val != "")
+				else
 				{
-					$scope.changeBgClr();
-				}
-			}
+					// Test diagonals
+					if (cellArray[1][1].val != "") 
+					{
+						if ((cellArray[0][0].val == cellArray[1][1].val &&
+					 		 cellArray[1][1].val == cellArray[2][2].val) ||
+							(cellArray[0][2].val == cellArray[1][1].val &&
+			 	 	 	 	 cellArray[1][1].val == cellArray[2][0].val)) 
+						{
+							$scope.changeBgClr();		
+						}
+					}
 
-			// Test rows.
-			for(var r = 1; r <= 3; ++r)
-			{
-				if(cellArray[r-1][0].val == cellArray[r-1][1].val && 
-		   	   	   cellArray[r-1][1].val == cellArray[r-1][2].val && 
-		   	   	   cellArray[r-1][0].val != "")
+					// Test columns.
+					for(var c = 1; c <= 3; ++c)
+					{
+						if(cellArray[0][c-1].val == cellArray[1][c-1].val  && 
+			   	   	   	   cellArray[1][c-1].val == cellArray[2][c-1].val  && 
+		   	   	   	   	   cellArray[0][c-1].val != "")
+						{
+							$scope.changeBgClr();
+						}
+					}
+
+					// Test rows.
+					for(var r = 1; r <= 3; ++r)
+					{
+						if(cellArray[r-1][0].val == cellArray[r-1][1].val && 
+		   	   	   	   	   cellArray[r-1][1].val == cellArray[r-1][2].val && 
+		   	   	   	   	   cellArray[r-1][0].val != "")
+						{
+					 		$scope.changeBgClr();
+						}
+					}
+
+					// Increment turn counter if the tests don't find a win.
+					turnObj.number++;
+
+					if (turnObj.number % 2 == 0)
+    				{
+    					$scope.radio = 2;
+    				}
+    				else
+    				{
+    					$scope.radio = 1;
+    				}
+				} 
+			};
+
+			$scope.changeBgClr = function() {
+
+				if ($scope.radio == 1) {
+					$scope.winStyle = {background:'#ffff11'};
+					$scope.resetVisible.view = true;
+				}
+
+				if ($scope.radio == 2) {
+					$scope.winStyle2 = {background:'#ffff11'};
+					$scope.resetVisible.view = true;
+				}
+			};
+
+			$scope.pressResetButton= function() {
+				$scope.resetStyle = {background: 'red'};
+				$scope.resetVisible.view = true;
+			};
+
+			$scope.resetGame = function() {
+
+				// Reset the radio buttons.
+				$scope.radio = 1;
+
+				// Set reset button to green.
+				$scope.resetStyle = {background: 'green'};
+
+				// Clear board.
+				for(var r = 1; r <=3; ++r)
 				{
-					$scope.changeBgClr();
+					for(var c = 1; c <= 3; ++c)
+					{	
+						$scope.ticTacToe[r-1][c-1].val = "";
+					}
 				}
-			}
 
-			// Increment turn counter if the tests don't find a win.
-			turnObj.number++;
+				// Hide reset popup window
+				$scope.resetVisible.view = false;
 
-			if (turnObj.number % 2 == 0)
-    		{
-    			$scope.radio = 2;
-    		}
-    		else
-    		{
-    			$scope.radio = 1;
-    		}
-		} 
-	};
+				// Reset radio button backgrounds
+				$scope.winStyle = {};
+    			$scope.winStyle2 = {};
 
-	$scope.changeBgClr = function() {
+			};
 
-		if ($scope.radio == 1) {
-			$scope.winStyle = {background:'#ffff11'};
-			$scope.resetVisible.view = true;
-		}
+			$scope.gameOver = function() {
+				$scope.resetVisible.view = false;
+				$scope.gameOverVisible.view = true;
+			};
 
-		if ($scope.radio == 2) {
-			$scope.winStyle2 = {background:'#ffff11'};
-			$scope.resetVisible.view = true;
-		}
-	};
-
-	$scope.pressResetButton= function() {
-		$scope.resetStyle = {background: 'red'};
-		$scope.resetVisible.view = true;
-	};
-
-	$scope.resetGame = function() {
-
-		// Reset the radio buttons.
-		$scope.radio = 1;
-
-		// Set reset button to green.
-		$scope.resetStyle = {background: 'green'};
-
-		// Clear board.
-		for(var r = 1; r <=3; ++r)
-		{
-			for(var c = 1; c <= 3; ++c)
-			{	
-				$scope.ticTacToe[r-1][c-1].val = "";
-			}
-		}
-
-		// Hide reset popup window
-		$scope.resetVisible.view = false;
-
-		// Reset radio button backgrounds
-		$scope.winStyle = {};
-    	$scope.winStyle2 = {};
-
-	};
-
-	$scope.gameOver = function() {
-		$scope.resetVisible.view = false;
-		$scope.gameOverVisible.view = true;
-	};
-
-  });
+  		});
+	});
+});
 
