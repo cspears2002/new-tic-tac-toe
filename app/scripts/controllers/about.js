11235fea155900696d1a4d9de77cf0126dca70d7
cspears2002 <cspears2002@yahoo.com>
2014-01-11 18:30:50 -0800

    Indented a few things.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -3,6 +3,6 @@
 angular.module('newTicApp')
   .controller('AboutCtrl', function ($scope, $location) {
   	$scope.blog = 'http://chrisspearswebdev.blogspot.com/';
-	$scope.twitterHandle = '@cspears2002';
-	$scope.linkedIn = 'http://www.linkedin.com/pub/christopher-spears/6/580/945'
+	  $scope.twitterHandle = '@cspears2002';
+	  $scope.linkedIn = 'http://www.linkedin.com/pub/christopher-spears/6/580/945'
   });
